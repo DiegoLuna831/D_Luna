de54e7f2878479373e4c29a2fa1fdef497783adb
DiegoLuna831 <L202423100@jilotepec.tecnm.mx>
2024-12-09 20:24:20 -0600

    Add files via upload

---
+++ Presentate.java
@@ -0,0 +1,7 @@
+public class Presentate {
+    public static void main(String[]Diego){
+        System.out.println("Nombre: Diego Luna");
+        System.out.println("Edad: 17 años de edad");
+        System.out.println("Pais: México");
+    }
+}
