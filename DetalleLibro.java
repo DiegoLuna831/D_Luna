e6120908d88b3be4f2646e4517f3cfcb27d4ec60
DiegoLuna831 <L202423100@jilotepec.tecnm.mx>
2024-12-11 15:35:07 -0600

    Add files via upload

---
+++ DetalleLibro.java
@@ -0,0 +1,19 @@
+public class DetalleLibro {
+    public static void main(String[]Diego){
+        //Detalle de un libro
+        String titulolibro = "el señor de los anillos";
+        int aniopublicacion=1945;
+        boolean libroDisponible=true;
+        double precio= 500.50;
+        //Imprimir el valor de las variables
+        System.out.println(titulolibro);
+        System.out.println(aniopublicacion);
+        System.out.println(libroDisponible);
+        System.out.println(precio);
+        //Modificar el titulo del libro
+        titulolibro="El señor de los Anillos";
+        System.out.println(titulolibro);
+
+
+    }
+}
---
+++ DetallePersona.java
@@ -0,0 +1,19 @@
+public class DetallePersona {
+    public static void main(String[] args) {
+        //Detalle de una persona
+String nombreCompleto="Diego Luna González";
+int edad=17;
+double altura = 1.78;
+String paisdeorigen="Mexico";
+char estadocivil;
+estadocivil='c'; //c-Casado/s-Soltero
+        //Imprimir los detalle de la persona
+        System.out.println(nombreCompleto);
+        System.out.println(edad);
+        System.out.println(altura);
+        System.out.println(paisdeorigen);
+        System.out.println(estadocivil);
+
+    }
+}
+
