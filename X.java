b74235ce42ced146cfd09fb23b8bc6f8c9f420d7
DiegoLuna831 <L202423100@jilotepec.tecnm.mx>
2024-12-17 20:45:25 -0600

    Add files via upload

---
+++ X.java
@@ -0,0 +1,18 @@
+public class X {
+    public class Mejor {
+        public static void main(String[] args) {
+            int a = 10;
+            int b = 20;
+            int c = 30;
+
+            if (a > b && a > c) {
+                System.out.println("El mayor es a: " + a);
+            } else if (b > a && b > c) {
+                System.out.println("El mayor es b: " + b);
+            } else {
+                System.out.println("El mayor es c: " + c);
+            }
+        }
+    }
+
+}
