5246c8788486cdf9f50df0bc7d65f46a30241650
DiegoLuna831 <L202423100@jilotepec.tecnm.mx>
2024-12-18 22:50:47 -0600

    Add files via upload

---
+++ TiendaLinea.java
@@ -0,0 +1,9 @@
+public class TiendaLinea {
+    public static void main(String[] args) {
+        //Detalle del producto
+        System.out.println("*** Tienda en linea (Detalle producto) ***");
+        String nombreproducto = "Laptop hp";
+        double precioProducto = 1000.50;
+        
+    }
+}
